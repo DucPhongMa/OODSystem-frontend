6552c3f44809d0c3e8a75d203def596cbd32feb3
Vivy Vuong <vngvy48@gmail.com>
2024-01-18 23:17:31 -0500

    authorization api

---
+++ src/app/api/auth.js
@@ -0,0 +1,51 @@
+const API_BACKEND = process.env.NEXT_PUBLIC_API_BACKEND_URL
+
+export const registerBusiness = async (user) => {
+  try {
+    const response = await fetch(`${API_BACKEND}api/auth/local/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: user.username,
+        email: user.email,
+        password: user.password,
+        role: "Business",
+      }),
+    })
+
+    const responseData = await response.json()
+    const item = {
+      value: responseData.jwt,
+      expiry: new Date().getTime() + 30 * 60000,
+    }
+    localStorage.setItem("authorization", JSON.stringify(item))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export const loginBusiness = async (user) => {
+  try {
+    const response = await fetch(`${API_BACKEND}api/auth/local`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: user.username,
+        password: user.password,
+      }),
+    })
+
+    const responseData = await response.json()
+    const item = {
+      value: responseData.jwt,
+      expiry: new Date().getTime() + 30 * 60000,
+    }
+    localStorage.setItem("authorization", JSON.stringify(item))
+  } catch (e) {
+    console.log(e)
+  }
+}
