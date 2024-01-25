44951210168d69802ca1a29d635ec3a6c6e1ce0a
YumeiWang <yumei.wang@mohawkcollege.ca>
2024-01-25 08:28:43 -0500

    add api and banner

--- src/app/(routes)/[route]/page.js
+++ src/app/(routes)/[route]/page.js
@@ -1,27 +1,53 @@
-"use client"
-import { useState, useEffect } from "react"
-import { useParams } from "next/navigation"
-import { getRestaurantByRoute } from "../../api/restaurant"
-import RestaurantAppBar from "@/app/components/RestaurantAppBar"
+'use client';
+import { useState, useEffect } from 'react';
+import { useParams } from 'next/navigation';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import Image from 'next/image';
+
+import { getRestaurantByRoute } from '../../api/restaurant';
+import RestaurantAppBar from '@/app/components/RestaurantAppBar';
 
 export default function RestaurantDetail() {
-  const [restaurantName, setRestaurantName] = useState("")
-  const params = useParams()
-    useEffect(() => {
-    const restaurantRoute = params.route
+  // const [restaurantName, setRestaurantName] = useState('');
+  const [restaurantInfo, setRestaurantInfo] = useState();
+  const params = useParams();
+  const restaurantRoute = params.route;
+
+  useEffect(() => {
     async function fetchMyAPI() {
       // not work right now because backend need to add field
-      const restaurantData = await getRestaurantByRoute(restaurantRoute)
-      console.log(restaurantData)
-      setRestaurantName(restaurantData.attributes.name)
+      const restaurantData = await getRestaurantByRoute(restaurantRoute);
+      console.log(restaurantData);
+      setRestaurantInfo(restaurantData.attributes);
+      // setRestaurantName(restaurantData.attributes.name);
     }
 
-    fetchMyAPI()
-  }, [])
+    fetchMyAPI();
+  }, []);
 
+  // console.log(restaurantInfo);
   return (
     <>
-      <RestaurantAppBar restaurantID={restaurantName} />
+      {!restaurantInfo && (
+        <Backdrop
+          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={restaurantInfo == null}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      )}
+      {restaurantInfo && (
+        <>
+          <RestaurantAppBar restaurantInfo={restaurantInfo} />
+          <Image
+            src="/Food_Banner.jpeg"
+            alt="banner example"
+            width={1500}
+            height={30}
+          />
+        </>
+      )}
     </>
   );
 }
--- src/app/components/RestaurantAppBar.js
+++ src/app/components/RestaurantAppBar.js
@@ -5,14 +5,13 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import AdbIcon from '@mui/icons-material/Adb';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import Link from 'next/link';
 import Image from 'next/image';
 
-const RestaurantAppBar = ({ restaurantID }) => {
-  console.log('?????');
-  console.log(restaurantID);
+
+const RestaurantAppBar = ({ restaurantInfo }) => {
+  // console.log(restaurantInfo);
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -27,41 +26,71 @@ const RestaurantAppBar = ({ restaurantID }) => {
           }}
         >
           <Typography variant="h6" component="div" sx={{ display: 'inline' }}>
-            Sushi: 1 A Street, Toronto, ON
+            {restaurantInfo.name}: {restaurantInfo.restaurant_contact.address}
           </Typography>
         </Box>
         <Toolbar>
-          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
           <div
-            sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-            }}
+            style={{ position: 'relative', width: 240 }}
           >
-            <Image src="/sushi.png" alt="sushi logo" width={100} height={100} />
-            <div>
+            <Image
+              src="/sushi.png"
+              alt="sushi logo"
+              width={100}
+              height={100}
+              priority={true}
+            />
+            <span
+              style={{
+                position: 'absolute',
+                bottom: 50,
+                right: 40,
+                color: 'white',
+              }}
+            >
+              Hours: 10:00 -
+            </span>
+            <span
+              style={{
+                position: 'absolute',
+                bottom: 30,
+                right: 50,
+                color: 'white',
+              }}
+            >
+              23:00
+            </span>
+            {/* <div>
               <Typography variant="h6" component="div">
                 4.5(198 ratings)
-              </Typography>
-              <Typography variant="body1" component="div">
-                Hours 10:00 AM - 11:00 PM
-              </Typography>
-            </div>
+              </Typography> */}
+            {/* <Typography variant="body1" component="div">
+              Hours 10:00 AM - 11:00 PM
+            </Typography> */}
+            {/* </div> */}
           </div>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link href={`/${restaurantID}`}>ORDER PICKUP</Link>
+            <Link href={`/${restaurantInfo.route}`}>ORDER PICKUP</Link>
           </Typography>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link href={`/${restaurantID}/about`}>ABOUT</Link>
+            <Link href={`/${restaurantInfo.route}/about`}>ABOUT</Link>
           </Typography>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link href={`/${restaurantID}/reviews`}>REVIEWS</Link>
+            <Link href={`/${restaurantInfo.route}/reviews`}>REVIEWS</Link>
           </Typography>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link href={`/${restaurantID}/orders`}>ORDERS</Link>
+            <Link href={`/${restaurantInfo.route}/orders`}>ORDERS</Link>
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" variant="outlined">
+            Log In
+          </Button>
+          <IconButton
+            color="inherit"
+            variant="outlined"
+            aria-label="add to shopping cart"
+          >
+            <AddShoppingCartIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
