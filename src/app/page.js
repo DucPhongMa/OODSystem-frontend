cafd460dd9ee153c42218dce6369a5a6ab9196d3
Vivy Vuong <vngvy48@gmail.com>
2024-02-26 17:42:43 +0000

    fixed the ci file and make sure the commit format code works as expected

--- src/app/page.js
+++ src/app/page.js
@@ -5,31 +5,19 @@ import { useState, useEffect } from "react";
 import { registerBusiness, loginBusiness } from "./api/auth";
 
 export default function Home() {
-  // useEffect(() => {
-  //   registerBusiness({
-  //     username: "Vivy",
-  //     email: "vngvy48@gmail.com",
-  //     password: "password1234.",
-  //   })
+  useEffect(() => {
+    registerBusiness({
+      username: "Vivy",
+      email: "vngvy48@gmail.com",
+      password: "password1234.",
+    })
 
-  //   // loginBusiness({
-  //   //   username: "Vivy",
-  //   //   password: "testing",
-  //   // })
-  // }, [])
-  // return <h1>Test API Develop</h1>
+    // loginBusiness({
+    //   username: "Vivy",
+    //   password: "testing",
+    // })
+  }, []);
 
-  // Test Restaurant Data
-  const [imageData, setImageData] = useState();
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    await addImage(imageData);
-  };
-
-  const handleImageFile = (event) => {
-    const input = event.target;
-    setImageData(input.files[0]);
-  };
   return (
     <form onSubmit={handleSubmit}>
       <label>
