7ad3b8bda68c5e9c7b56e408b133845410f3fe4a
Vivy Vuong <vngvy48@gmail.com>
2024-02-26 21:18:52 -0500

    changed order to allow add order from non-logged in user

--- src/app/api/order.js
+++ src/app/api/order.js
@@ -1,7 +1,17 @@
 const API_BACKEND = process.env.NEXT_PUBLIC_API_BACKEND_URL;
-export const addOrder = async (dishesArray, userID, restaurantID, note) => {
+export const addOrder = async (
+  matchID,
+  dishesArray,
+  restaurantID,
+  note,
+  username,
+  phoneNumber,
+  userID
+) => {
   const orderDetailIDs = [];
   let orderTotal = 0;
+  let userCheckIn;
+  let submittedOrder;
   // persist menu_items to database;
 
   for (const dish of dishesArray) {
@@ -27,6 +37,10 @@ export const addOrder = async (dishesArray, userID, restaurantID, note) => {
       });
   }
 
+  if (userID) {
+    userCheckIn = true;
+  }
+
   // persist order to database
   await fetch(`${API_BACKEND}api/orders`, {
     method: "POST",
@@ -38,19 +52,37 @@ export const addOrder = async (dishesArray, userID, restaurantID, note) => {
       data: {
         time_placed: Date.now(),
         note: note,
+        matchID: matchID,
         total_price: orderTotal,
         tax: orderTotal + orderTotal * 0.13,
         order_details: orderDetailIDs,
         restaurant: restaurantID,
-        users_permissions_user: userID,
+        users_permissions_user: userCheckIn ? userID : null,
+        username: userCheckIn ? null : username,
+        phone_number: userCheckIn ? null : phoneNumber,
         status: "pending",
       },
     }),
   })
     .then((res) => res.json())
     .then((jsonData) => {
-      return jsonData;
+      submittedOrder = jsonData;
     });
+
+  return submittedOrder;
+};
+
+export const getOrderByUUID = async (uuid) => {
+  let order;
+  await fetch(
+    `${API_BACKEND}api/orders?filters[matchID][$eq]=${uuid}&populate[order_details][populate][0]=menu_item&populate[users_permissions_user]=*`
+  )
+    .then((res) => res.json())
+    .then((jsonData) => {
+      order = jsonData.data[0];
+    });
+
+  return order;
 };
 
 export const getOrderBasedOnStatus = async (restaurantID, status) => {
