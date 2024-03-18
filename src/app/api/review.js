d3cb8ee862e34e228b01ad0d2f89ee5a8567203e
maducphong7@gmail.com <maducphong1998>
2024-03-18 02:23:14 -0400

    add api integration for review

---
+++ src/app/api/review.js
@@ -0,0 +1,14 @@
+const API_BACKEND = process.env.NEXT_PUBLIC_API_BACKEND_URL;
+
+export const getAllReviews = async (restaurantRoute) => {
+    let reviews;
+    await fetch(
+      `${API_BACKEND}api/reviews?filters[restaurant][route][$eq]=${restaurantRoute}&populate=*`
+    )
+      .then((res) => res.json())
+      .then((jsonData) => {
+        reviews = jsonData.data;
+      });
+  
+    return reviews;
+  };
