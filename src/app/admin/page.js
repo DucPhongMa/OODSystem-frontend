30c4de515c7bae6ee24e17af10dc8b24bea54eb9
Katie Liu <kliu57@myseneca.ca>
2024-10-09 11:40:31 -0400

    fixed responsive issues with landing page

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { Typography, Grid, Button, Box } from "@mui/material";
+import { Typography, Grid, Button, Box, Container } from "@mui/material";
 import MainNavbar from "../components/admin/register/LandingNavbar";
 import styles from "../styles/AdminLanding.module.scss";
 import Image from "next/image";
@@ -70,135 +70,143 @@ export default function Landing() {
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-          padding: "0 150px",
-          marginTop: "-116px",
+          mt: -8,
         }}
       >
-        <Grid container justifyContent="center" alignItems="center">
-          <Grid item xs={12} sm={6} md={5} lg={5}>
-            <Box
-              sx={{
-                padding: "0 20px",
-                textAlign: "left",
-                width: "100%",
-              }}
-            >
-              <div className={styles.textContainer}>
+        <Container
+          maxWidth="lg"
+          sx={{
+            padding: 0,
+            maxWidth: {
+              xs: "80%",
+              sm: "80%",
+              md: "90%",
+              lg: "90%",
+              xl: "80%",
+            },
+          }}
+        >
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+              <Box>
+                <div className={styles.textContainer}>
+                  <Typography
+                    component="div"
+                    fontSize={{ xs: "2rem", sm: "3.75rem" }}
+                    variant="h4"
+                    sx={{
+                      fontFamily: "Raleway, Arial, sans-serif",
+                      fontWeight: 600,
+                      color: "#92888b",
+                    }}
+                    className={`${styles.animatedText} ${styles[word.animation]}`}
+                  >
+                    {word.text}
+                  </Typography>
+                </div>
                 <Typography
-                  component="div"
-                  fontSize={"3.75rem"}
+                  fontSize={{ xs: "2rem", sm: "3.75rem" }}
                   variant="h4"
                   sx={{
-                    marginBottom: "50px",
+                    marginBottom: { xs: "5px", sm: "14px" },
                     fontFamily: "Raleway, Arial, sans-serif",
                     fontWeight: 600,
-                    color: "#92888b",
                   }}
-                  className={`${styles.animatedText} ${styles[word.animation]}`}
                 >
-                  {word.text}
+                  Restaurante Wiz
                 </Typography>
-              </div>
-              <Typography
-                fontSize={"3.75em"}
-                variant="h4"
-                sx={{
-                  marginBottom: "14px",
-                  fontFamily: "Raleway, Arial, sans-serif",
-                  fontWeight: 600,
-                }}
-              >
-                Restaurante Wiz
-              </Typography>
-              <Typography
-                fontSize={"1.2rem"}
-                variant="body1"
-                sx={{
-                  fontFamily: "Raleway, Arial, sans-serif",
-                  fontWeight: 400,
-                }}
-              >
-                Transform your restaurant into a stunning website with a
-                built-in pickup order system and advanced design features
-                &mdash; just a click away.
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "flex-start",
-                  marginTop: "33px",
-                  fontFamily: "Raleway, Arial, sans-serif",
-                  fontWeight: 400,
-                }}
-              >
-                <Button
-                  href="/admin/register"
-                  variant="contained"
+                <Typography
+                  fontSize={"1.2rem"}
+                  variant="body1"
                   sx={{
-                    backgroundColor: "#bab4b6",
-                    marginRight: "9px",
-                    width: "122px",
-                    height: "52px",
                     fontFamily: "Raleway, Arial, sans-serif",
-                    fontWeight: '600',
-                    textTransform: "none",
-                    color: "#050405",
-                    transition:
-                      "background-color 0.2s ease-in-out, color 0.2s ease-in-out, border-color 0.2s ease-in-out",
+                    fontWeight: 400,
+                  }}
+                >
+                  Transform your restaurant into a stunning website with a
+                  built-in pickup order system and advanced design features
+                  &mdash; just a click away.
+                </Typography>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    marginTop: "33px",
+                    fontFamily: "Raleway, Arial, sans-serif",
+                    fontWeight: 400,
+                  }}
+                >
+                  <Button
+                    href="/admin/register"
+                    variant="contained"
+                    sx={{
+                      backgroundColor: "#bab4b6",
+                      marginRight: "9px",
+                      width: "122px",
+                      height: "52px",
+                      fontFamily: "Raleway, Arial, sans-serif",
+                      fontWeight: "600",
+                      textTransform: "none",
+                      color: "#050405",
+                      transition:
+                        "background-color 0.2s ease-in-out, color 0.2s ease-in-out, border-color 0.2s ease-in-out",
 
-                    "&:hover": {
-                      backgroundColor: "#757173",
-                      color: "#fefcfc",
-                      transition: "none"
-                    },
-                  }}
-                >
-                  Get started
-                </Button>
-                <Button
-                  href="https://ood-system-frontend.vercel.app/ryo"
-                  variant="contained"
-                  sx={{
-                    backgroundColor: "transparent",
-                    border: "2px solid #bab4b6",
-                    width: "122px",
-                    height: "52px",
-                    fontFamily: "Raleway, Arial, sans-serif",
-                    fontWeight: '600',
-                    textTransform: "none",
-                    color: "#bab4b6",
-                    transition:
-                      "background-color 0.3s ease, color 0.3s ease, border-color 0.3s ease",
-                    "&:hover": {
-                      backgroundColor: "rgba(186, 180, 182, 0.33)",
-                      border: "none",
-                      color: "#fefcfc",
-                      transition: "none"
-                    },
-                  }}
-                >
-                  See product
-                </Button>
+                      "&:hover": {
+                        backgroundColor: "#757173",
+                        color: "#fefcfc",
+                        transition: "none",
+                      },
+                    }}
+                  >
+                    Get started
+                  </Button>
+                  <Button
+                    href="https://ood-system-frontend.vercel.app/ryo"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    variant="contained"
+                    sx={{
+                      backgroundColor: "transparent",
+                      border: "2px solid #bab4b6",
+                      width: "122px",
+                      height: "52px",
+                      fontFamily: "Raleway, Arial, sans-serif",
+                      fontWeight: "600",
+                      textTransform: "none",
+                      color: "#bab4b6",
+                      transition:
+                        "background-color 0.3s ease, color 0.3s ease, border-color 0.3s ease",
+                      "&:hover": {
+                        backgroundColor: "rgba(186, 180, 182, 0.33)",
+                        border: "none",
+                        color: "#fefcfc",
+                        transition: "none",
+                      },
+                    }}
+                  >
+                    See product
+                  </Button>
+                </Box>
               </Box>
-            </Box>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} lg={6}>
+              <Box>
+                <Image
+                  src="/landingAd.jpg"
+                  alt="Mobile website"
+                  layout="responsive"
+                  width={580}
+                  height={354}
+                />
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6} md={5} lg={5}>
-            <Box
-              sx={{
-                maxWidth: "100%",
-                padding: "0 20px",
-              }}
-            >
-              <Image
-                src="/landingAd.jpg"
-                alt="Mobile website"
-                layout="responsive"
-                width={580}
-                height={354}
-              />
-            </Box>
-          </Grid>
-        </Grid>
+        </Container>
       </Box>
     </div>
   );
